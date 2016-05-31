8565b9e6c73de0d12c54b4d37c595fe586fdccb3
islxyqwe <islxyqwe123@gmail.com>
2016-05-31 20:55:41 +0800

    score calculator!

---
+++ src/calc.py
@@ -0,0 +1,158 @@
+# song=[[note= int(location in fraps)...],diffcultyrate,color=0,1,2,3(all,cute,cool,passion)]
+# note=[[socre,happeningrate]...]
+# bouns=[bounsrate,happeningrate]
+# bounses=[bouns...]
+# totalbouns=bouns(cbbouns*sbbouns)
+import skill,unit
+def samplesong():
+	notes=[]
+	for i in range(1,601):
+		notes.append(i*12)
+	return [notes,1.9,0]
+def bounsappend(bounses,bounsrate,happeningrate):
+	newrate=0
+	for bouns in bounses:
+		if bouns[0]<bounsrate:
+			newrate+=bouns[1]*happeningrate
+			bouns[1]-=bouns[1]*happeningrate
+		if bouns[0]==bounsrate:
+			bouns[1]+=newrate
+			newrate=0
+			break
+	if newrate>0:
+		bounses.append([bounsrate,newrate])
+	bounses.sort(key=lambda x:x[0])
+	return bounses
+def calctotalbouns(bounses1,bounses2):
+	totalbouns=[]
+	for bounsl in bounses1:
+		for bounsr in bounses2:
+			totalbouns.append([bounsl[0]*bounsr[0],bounsl[1]*bounsr[1]])
+	totalbouns.sort(key=lambda x:x[0])
+	return totalbouns
+def calcE(PDF):
+	sum=0
+	for x in PDF:
+		sum+=x[0]*x[1]
+	return sum
+def calcEs(PDFs):
+	res=[]
+	for PDF in PDFs:
+		res.append(calcE(PDF))
+	return res
+def PDF2CDF(PDF):
+	CDF=[]
+	sumrate=0
+	for x in PDF:
+		sumrate+=x[1]
+		CDF.append([x[0],sumrate])
+	return CDF
+def CDF2PDF(CDF):
+	PDF=[]
+	sumrate=0
+	for x in CDF:
+		PDF.append([x[0],x[1]-sumrate])
+		sumrate=x[1]
+	return PDF
+def calcmax(PDF,times):
+	CDF=PDF2CDF(PDF)
+	for x in CDF:
+		x[1]=x[1]**times
+	return CDF2PDF(CDF)
+def bounsofnote(location,skills):
+	cbbouns=[[1,1]]
+	sbbouns=[[1,1]]
+	for skill in skills:
+		if location%skill.interval<=skill.last:
+			if skill.type:
+				sbbouns=bounsappend(sbbouns,1+skill.effect/100,skill.rate/100)
+			else:
+				cbbouns=bounsappend(cbbouns,1+skill.effect/100,skill.rate/100)
+	totalbouns=calctotalbouns(sbbouns,cbbouns)
+	return totalbouns
+def calcnotescore(standardscore,totalbouns,combobouns):
+	note=[]
+	for b in totalbouns:
+		note.append([b[0]*standardscore*combobouns//1,b[1]])
+	return note
+def calcscore(song,unit):
+	maxnote=len(song[0])
+	standard=unit.appeal*song[1]/maxnote
+	note=0
+	combobouns=1
+	notes=[]
+	for n in song[0]:
+		note+=1
+		if note==maxnote//20:
+			combobouns=1.1
+		if note==maxnote//10:
+			combobouns=1.2
+		if note==maxnote//4:
+			combobouns=1.3
+		if note==maxnote//2:
+			combobouns=1.4
+		if note==int(maxnote*0.7):
+			combobouns=1.5
+		if note==int(maxnote*0.8):
+			combobouns=1.7
+		if note==int(maxnote*0.9):
+			combobouns=2.0
+		notes.append(calcnotescore(standard,bounsofnote(n,unit.skills),combobouns))
+	return notes
+def anylizeskill(song,unit):
+	bounses=[]
+	for n in song[0]:
+		bounses.append(bounsofnote(n,unit.skills))
+	return bounses
+def calclive(song,unit,times):
+	notes=calcscore(song,unit)
+	avgscore=0
+	avgmaxscore=0
+	for n in notes:
+		avgscore+=calcE(n)
+		avgmaxscore+=calcE(calcmax(n,times))
+	print("期望得分="+str(avgscore))
+	print(str(times)+"次的期望最大得分="+str(avgmaxscore))
+def skillcoverage(data):
+	score=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	rating=[
+	"无覆盖(F)","几乎无覆盖(E)","基本无覆盖(E+)",
+	"非常差(D-)","差(D)","比较差(D+)",
+	"略差(C-)","一般(C)","稍好(C+)",
+	"比较良好(B-)","良好(B)","十分良好(B+)",
+	"比较优秀(A-)","优秀(A)","十分优秀(A+)",
+	"出色(S)","完美(SS)"
+	]
+	s=""
+	for i in data:
+		score[int((i-1)/0.02)]+=1
+	for i in range(0,17):
+		if score[i]>0:
+			s=s+rating[i]+":"+str(score[i])+"("+str(int(100*score[i]/len(data)))+"%)\n"
+	avg=sum(data)/len(data)
+	s=s+"总评："+rating[int((avg-1)/0.02)]+"\n平均倍率:"+str(avg)
+	return s
+def plotdata(PDFs):
+	i=0
+	totalx=[]
+	totaly=[]
+	totalz=[]
+	for PDF in PDFs:
+		x=[]
+		y=[]
+		z=[]
+		i+=1
+		last=0
+		CDF=PDF2CDF(PDF)
+		for d in CDF:
+			x.append(i)
+			x.append(i)
+			y.append(last)
+			y.append(d[1])
+			z.append(d[0])
+			z.append(d[0])
+			last=d[1]
+		totalx.append(x)
+		totaly.append(y)
+		totalz.append(z)
+	return [totalx,totaly,totalz]	
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sim,skill,unit,centerskill,idol,team
-import nn
+#import nn
+import calc
 low=1
 med=2
 high=3
@@ -10,17 +11,32 @@ s2=skill.skill(sb,9,5,15,10,med)
 s3=skill.skill(cb,11,5,15,10,high)
 s4=skill.skill(sb,13,6,17,10,high)
 s5=skill.skill(sb,7,4,15,10,med)
-i1=idol.idol("mio",s1,centerskill.centerskill("all","vo",48),6115,3854,3137,"pa",39)
-i2=idol.idol("natalia",s2,centerskill.centerskill("pa","vo",60),6008,2991,3707,"pa",39)
-i3=idol.idol("airi",s3,centerskill.centerskill("pa","vo",90),6954,4564,3779,"pa",44)
-i4=idol.idol("aiko",s4,centerskill.centerskill("pa","vo",90),6982,4596,3812,"pa",44)
-i5=idol.idol("mika",s5,centerskill.centerskill("pa","vo",60),5729,3166,3806,"pa",39)
+s6=skill.skill(cb,13,6,12,10,high)
+i1=idol.idol("mio",s1,centerskill.centerskill("all","vo",48),6115,3137,3854,"pa",39)
+i2=idol.idol("natalia",s2,centerskill.centerskill("pa","vo",60),6008,3707,2991,"pa",39)
+i3=idol.idol("airi",s3,centerskill.centerskill("pa","vo",90),6954,3779,4564,"pa",44)
+i4=idol.idol("aiko",s4,centerskill.centerskill("pa","vo",90),6982,3812,4596,"pa",44)
+i5=idol.idol("mika",s5,centerskill.centerskill("pa","vo",60),5729,3806,3166,"pa",39)
+i6=idol.idol("kaoru",s6,centerskill.centerskill("pa","vo",60),6061,3228,3921,"pa",39)
 songbouns=centerskill.centerskill("all","appeal",30)+centerskill.centerskill("all","skill",30)
 cubouns=centerskill.centerskill("cu","appeal",10)
 cobouns=centerskill.centerskill("co","appeal",10)
 pabouns=centerskill.centerskill("pa","appeal",10)
 t=team.team([i3,i2,i4,i1,i5],i4,99798,songbouns,cubouns,cobouns,pabouns)
 u=t.unit
-#u=unit.unit(288017,[s1,s2,s3,s4,s5])
-print(nn.simlivenn(u))
-print(sim.simlivetest(u,25))
+#u=unit.unit(294969,[s6,s7,s8,s9,s10])
+#print(nn.simlivenn(u))
+#print(sim.simlivetest(u,25))
+calc.calclive(calc.samplesong(),u,25)
+notebouns=calc.anylizeskill(calc.samplesong(),u)
+res=calc.calcEs(notebouns)
+print("技能分析：")
+print(calc.skillcoverage(res))
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# res=calc.plotdata(notebouns)
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# for i in range(0,len(res[0])):
+	# ax.plot(res[0][i], res[1][i], res[2][i], label='parametric curve')
+# plt.show()
